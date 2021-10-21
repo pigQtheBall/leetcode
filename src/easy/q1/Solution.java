fc47c65e0ddb978d208860e07d779cadaed85399
pigQtheBall <twoperrr@gmail.com>
2021-10-21 10:20:52 -0400

    oct.20

--- src/easy/q1/Solution.java
+++ src/easy/q1/Solution.java
@@ -5,16 +5,19 @@ import java.util.Map;
 
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int[] result = new int[2];
+        int[] output = new int[2];
+        // key: numbers, value: index
+        // key has to stand for numbers and value has to be index since we would use the map.get(Key) method to find
+        // the index because what we want is the index as an output
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int i = 0; i < numbers.length; i++) {
             if (map.containsKey(target - numbers[i])) {
-                result[1] = i;
-                result[0] = map.get(target - numbers[i]);
-                return result;
+                output[1] = i;
+                output[0] = map.get(target - numbers[i]);
+                return output;
             }
             map.put(numbers[i], i);
         }
-        return result;
+        return output;
     }
 }
