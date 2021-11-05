a88e2e304607aaeb584d64520dea809d0b831cc9
pigQtheBall <twoperrr@gmail.com>
2021-11-05 12:02:29 -0400

    nov.5 Q55

--- src/medium/q55/Solution.java
+++ src/medium/q55/Solution.java
@@ -1,4 +1,15 @@
 package medium.q55;
 
 public class Solution {
+    public boolean canJump(int[] nums) {
+        // save the furthest index we can jump to
+        int d = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(i > d) return false;
+            d = Math.max(d, i + nums[i]);
+            if(d >= nums.length-1) return true;
+        }
+        return true;
+    }
+
 }
