0c70712bedacf3a9651cef9bea8ffb8a7f2f9360
pigQtheBall <twoperrr@gmail.com>
2021-11-13 17:40:39 -0500

    nov.13 Q35 review

--- src/easy/q35/Solution.java
+++ src/easy/q35/Solution.java
@@ -2,8 +2,13 @@ package easy.q35;
 
 public class Solution {
     public int searchInsert(int[] nums, int target) {
+        // when target is larger than all the element in nums
+        // insert target at last
         if(target > nums[nums.length - 1]) return nums.length;
+        // when target is smaller than all the elment in nums
+        // insert target at the beginning of the array
         if(target < nums[0]) return 0;
+        // binary search
         int middle = 0;
         int left = 0;
         int right = nums.length - 1;
