ca14d9d3fc27cfd94b9d9e5b2f6e9b8874b49836
pigQtheBall <twoperrr@gmail.com>
2021-11-16 15:41:55 -0500

    nov.16 Q206 recursive

--- src/easy/q206/Solution.java
+++ src/easy/q206/Solution.java
@@ -2,6 +2,38 @@ package easy.q206;
 import leetcode.adt.ListNode;
 
 public class Solution {
+    public ListNode reverseList2(ListNode head) {
+        ListNode prev = null;
+        ListNode cur = head;
+        return reverse(prev, cur);
+    }
+
+    public ListNode reverse(ListNode prev, ListNode cur){
+        if(cur == null) return prev;
+        ListNode tmp = cur.next;
+        cur.next = prev;
+        prev = cur;
+        cur = tmp;
+        return reverse(prev, tmp);
+
+
+    }
+
+    public ListNode reverseList3(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        ListNode prev = null;
+        ListNode cur = head;
+        while(cur != null){
+            ListNode tmp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = tmp;
+        }
+        return prev;
+    }
+
     public ListNode reverseList(ListNode head) {
         if(head == null){
             return null;
