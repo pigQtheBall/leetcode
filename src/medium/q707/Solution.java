ba972a1077a9df84be08b3a0a263f66dfe5d7107
pigQtheBall <twoperrr@gmail.com>
2021-11-16 15:21:18 -0500

    nov.16 Q707

--- src/medium/q707/Solution.java
+++ src/medium/q707/Solution.java
@@ -1,82 +1,72 @@
 package medium.q707;
 
+import java.util.List;
+
 public class Solution {
     class MyLinkedList {
-        int val;
-        MyLinkedList next;
+        int size;
+        ListNode head;
+
+        class ListNode {
+            int val;
+            ListNode next;
+            ListNode(int x) { val = x; }
+        }
 
         public MyLinkedList() {
+            size = 0;
+            head = new ListNode(0);
         }
 
-        public MyLinkedList(int val){
-            this.val = val;
-            this.next = null;
-        }
-
-        public MyLinkedList(int val, MyLinkedList next){
-            this.val = val;
-            this.next = next;
-        }
 
         public int get(int index) {
-            if(index == 0) return this.val;
-            int ct = 1;
-            MyLinkedList tmp = this.next;
-            while(tmp != null){
-                if(ct == index) return tmp.val;
-                tmp = tmp.next;
+            if(index < 0 || index >= size) return -1;
+            ListNode cur = this.head;
+            int ct = 0;
+            while(ct < index){
+                cur = cur.next;
                 ct++;
             }
-            return -1;
-        }
-
-        public MyLinkedList getNode(int index) {
-            if(index < 0) return null;
-            if(index == 0) return this;
-            int ct = 1;
-            MyLinkedList tmp = this.next;
-            while(tmp != null){
-                if(ct == index) return tmp;
-                tmp = tmp.next;
-                ct++;
-            }
-            return null;
+            return cur.next.val;
         }
 
         public void addAtHead(int val) {
-            MyLinkedList hd = new MyLinkedList(val);
-            hd.next = this;
+            this.addAtIndex(0, val);
         }
 
         public void addAtTail(int val) {
-            MyLinkedList tl = new MyLinkedList(val);
-            if(this.next == null) this.next = tl;
-            MyLinkedList cur = this;
-            while(cur.next != null){
-                cur = cur.next;
-            }
-            cur.next = tl;
+            this.addAtIndex(size, val);
 
         }
 
         public void addAtIndex(int index, int val) {
-            MyLinkedList el = new MyLinkedList(val);
-            MyLinkedList prev = this.getNode(index - 1);
-            MyLinkedList after = this.next; // = this.getNode(index + 1);
-            if(prev != null){
-                after = prev.next.next;
-                prev.next = el;
+            if(index > size) return;
+            if(index < 0) index = 0;
+            ListNode add = new ListNode(val);
+            this.size++;
+            if(index == 0){
+                add.next = this.head;
             }
-            el.next = after;
+            int ct = 0;
+            ListNode cur = this.head;
+            while(ct < index){
+                cur = cur.next;
+                ct++;
+            }
+            add.next = cur.next;
+            cur.next = add;
         }
 
         public void deleteAtIndex(int index) {
-            MyLinkedList prev = this.getNode(index - 1);
-            MyLinkedList after = this.next;
-            if(prev != null){
-                after = prev.next.next;
-                prev.next = after;
+            if (index < 0 || index >= size) return;
+            this.size--;
+            int ct = 0;
+            ListNode cur = this.head;
+            while(ct < index){
+                cur = cur.next;
+                ct++;
             }
+            cur.next = cur.next.next;
 
         }
     }
