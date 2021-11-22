4d661a380302699f9bfc41061ba369865bf75b37
pigQtheBall <twoperrr@gmail.com>
2021-11-22 12:40:53 -0500

    nov.22 Q18

---
+++ src/medium/q18/Solution.java
@@ -0,0 +1,38 @@
+package medium.q18;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        // sort the given list first
+        Arrays.sort(nums);
+
+        for(int i=0; i<nums.length; i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1; j<nums.length; j++){
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
+                int cur = nums[i] + nums[j];
+                int left = j+1;
+                int right = nums.length - 1;
+
+                while(left < right){
+                    int sum = cur + nums[left] + nums[right];
+                    if (sum > target) right--;
+                    else if (sum < target) left++;
+                    else {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+
+                        left++;
+                        right--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
