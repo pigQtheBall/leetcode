bd1dea26cd3bbb5dc30e328eb83b21b85d52022c
pigQtheBall <twoperrr@gmail.com>
2021-12-23 13:26:11 -0500

    Dec.23 Q66 reviewing

--- src/easy/q66/Solution.java
+++ src/easy/q66/Solution.java
@@ -1,6 +1,28 @@
 package easy.q66;
 
 public class Solution {
+    // 3rd try
+    public int[] plusOne3(int[] digits) {
+        int n =digits.length;
+        int last = digits[n - 1];
+        if(last < 9){
+            digits[n-1]++;
+            return digits;
+        }
+        int i = 1;
+        while( n-i >= 0 && digits[n-i] == 9){
+            digits[n-i] = 0;
+            i++;
+        }
+        if(n-i<0){
+            int[] out = new int[n+1];
+            out[0] = 1;
+            return out;
+        }
+        digits[n-i]++;
+        return digits;
+    }
+
     // 2nd try
     public int[] plusOne2(int[] digits) {
         int n =digits.length;
