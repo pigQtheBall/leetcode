162da20b008520546382ff4a01d50f0e6824f75e
pigQtheBall <twoperrr@gmail.com>
2022-01-03 12:04:34 -0500

    Jan.3 reviewing

--- src/easy/Qmian207/Solution.java
+++ src/easy/Qmian207/Solution.java
@@ -20,4 +20,39 @@ public class Solution {
         }
         return null;
     }
+
+    public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
+        ListNode curA = headA;
+        ListNode curB = headB;
+        int lenA = 0;
+        int lenB = 0;
+        while(curA != null){
+            lenA++;
+            curA = curA.next;
+        }
+        while(curB != null){
+            lenB++;
+            curB = curB.next;
+        }
+        curA = headA;
+        curB = headB;
+        if(lenB > lenA){
+            int tmp = lenA;
+            lenA = lenB;
+            lenB = tmp;
+            curA = headB;
+            curB = headA;
+        }
+        int gap = lenA - lenB;
+        while(gap > 0){
+            curA = curA.next;
+            gap--;
+        }
+        while(curA != null){
+            if(curA == curB) return curA;
+            curA = curA.next;
+            curB = curB.next;
+        }
+        return null;
+    }
 }
