93fa0f257aa855af1e35fc75c928a6b5cdbb2341
pigQtheBall <twoperrr@gmail.com>
2022-01-03 11:44:16 -0500

    Jan.3 Q19 reviewing

--- src/medium/q19/Solution.java
+++ src/medium/q19/Solution.java
@@ -3,6 +3,24 @@ package medium.q19;
 import leetcode.adt.ListNode;
 
 public class Solution {
+    // second try
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode right = head;
+        int ct = 0;
+        while(ct < n){
+            right = right.next;
+            ct++;
+        }
+        ListNode left = head;
+        if(right == null) return left.next;
+        while(right.next != null){
+            left = left.next;
+            right = right.next;
+        }
+        left.next = left.next.next;
+        return head;
+    }
+
     public ListNode removeNthFromEnd(ListNode head, int n) {
         int ct = 0;
         ListNode left = new ListNode(-1);
